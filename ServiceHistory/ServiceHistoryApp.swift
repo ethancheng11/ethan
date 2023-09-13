394639f06972163ddda707296acba92f4bf891e3
etan <ethan.cheng.sh@gmail.com>
2023-09-14 01:30:30 +1000

    Initial Commit

---
+++ ServiceHistory/ServiceHistoryApp.swift
@@ -0,0 +1,18 @@
+//
+//  ServiceHistoryApp.swift
+//  ServiceHistory
+//
+//  Created by ethan on 14/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct ServiceHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        
+        }
+    }
+}
